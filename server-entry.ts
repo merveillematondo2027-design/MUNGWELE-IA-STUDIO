77c267988e38f93710fdc12c53cbf7add271a880
merveillematondo2027-design <merveillematondo2027@gmail.com>
2026-08-30 14:34:41 +0200

    Make background server entry compatible with bundled CJS

--- server-entry.ts
+++ server-entry.ts
@@ -1,4 +1,4 @@
 import { installBackgroundGenerationPersistence } from './server/backgroundGenerationPersistence';
 
 installBackgroundGenerationPersistence();
-await import('./server');
+void import('./server');
