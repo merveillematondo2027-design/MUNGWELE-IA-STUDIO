73a8d84198d69192159769683d3ed21e8e87b619
merveillematondo2027-design <merveillematondo2027@gmail.com>
2026-08-30 21:12:05 +0100

    Install media download endpoint before app routes

--- server-entry.ts
+++ server-entry.ts
@@ -1,4 +1,6 @@
 import { installBackgroundGenerationPersistence } from './server/backgroundGenerationPersistence';
+import { installMediaDownload } from './server/installMediaDownload';
 
 installBackgroundGenerationPersistence();
+installMediaDownload();
 void import('./server');
