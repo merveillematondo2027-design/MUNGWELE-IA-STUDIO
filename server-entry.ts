152283e714119a47d7e80fc0b6c87bad1ce723f9
merveillematondo2027-design <merveillematondo2027@gmail.com>
2026-09-05 20:13:36 +0100

    feat: install Market-Cash payment proxy

--- server-entry.ts
+++ server-entry.ts
@@ -1,6 +1,8 @@
 import { installBackgroundGenerationPersistence } from './server/backgroundGenerationPersistence';
 import { installMediaDownload } from './server/installMediaDownload';
+import { installMarketCashPaymentProxy } from './server/marketCashPaymentProxy';
 
 installBackgroundGenerationPersistence();
 installMediaDownload();
+installMarketCashPaymentProxy();
 void import('./server');
