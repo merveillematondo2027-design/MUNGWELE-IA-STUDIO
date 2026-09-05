85277a2abf9f5d6005417b95a51537fd2bfcbcc2
merveillematondo2027-design <merveillematondo2027@gmail.com>
2026-09-06 00:27:03 +0100

    Install saved payment methods and auto-renew billing

--- server-entry.ts
+++ server-entry.ts
@@ -1,8 +1,10 @@
 import { installBackgroundGenerationPersistence } from './server/backgroundGenerationPersistence';
 import { installMediaDownload } from './server/installMediaDownload';
 import { installMarketCashPaymentProxy } from './server/marketCashPaymentProxy';
+import { installMarketCashBilling } from './server/installMarketCashBilling';
 
 installBackgroundGenerationPersistence();
 installMediaDownload();
 installMarketCashPaymentProxy();
+installMarketCashBilling();
 void import('./server');
